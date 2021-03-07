ea9a3fb8872091c94820b1d6cd8c0e767d21c32a
Kelly Koh <kellykoh@usc.edu>
2021-03-07 11:09:37 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-// Display Med List
-document.addEventListener("DOMContentLoaded", () => {
+ 
+ document.addEventListener("DOMContentLoaded", () => {
   const app = new Medication();
+  // Must call this function after add any medications.
   const refreshMedList = () => {
     document.querySelector("#med-list").innerHTML = Medication.all
-      .map((med) => `<li>Name: ${med.name} ID #${med.id} Start Date: ${med.startDate} Frequency: ${med.frequency}</li>`)
+      .map((med) => `<li>${med.name} #${med.id} Date: ${med.startDate} Frequency: ${med.frequency}</li>`)
       .join("\n");
-    // console.log("Added med", Medication.all);
+    console.log("Added med", Medication.all);
 
   };
 
   // Adds Medications
   const addMedForm = document.querySelector("#add-med");
-  // console.log('adding event listener');
+  console.log('adding event listener');
   addMedForm.addEventListener("submit", (submitMed) => {
+    console.log('helloo');
     submitMed.preventDefault();
-    // console.log('helloo');
     const name = document.querySelector("#add-med-name").value;
     const startDate = document.querySelector("#add-med-date").value;
     const frequency = document.querySelector("#add-med-freq").value;
@@ -25,7 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
-
 document.querySelector("#joke").onclick = function() {
   // jQuery's AJAX
   $.ajax({
@@ -33,7 +33,6 @@ document.querySelector("#joke").onclick = function() {
     // the endpoint
     url: "https://icanhazdadjoke.com/",
     headers: {"Accept": 'application/json'},
-    // the parameters to send to itunes API
     })
     .done(function(results) {
       // when a response is recieved this code runs
@@ -45,7 +44,7 @@ document.querySelector("#joke").onclick = function() {
       console.log("ERROR");
     });
 }
-
 function displayResults(results) {
   $("#display-joke").text(results.joke);
 }
+
